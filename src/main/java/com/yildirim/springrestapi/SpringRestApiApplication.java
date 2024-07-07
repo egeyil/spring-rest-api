d167ff97bfd2825e6d94ebc0d80feb3622beb53f
egeyil <egeyildirimdevelopment@gmail.com>
2024-07-07 21:52:47 +0200

    Initial project creation

---
+++ src/main/java/com/yildirim/springrestapi/SpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.yildirim.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiApplication.class, args);
+    }
+
+}
